b33590093401dc654194324cf340c8a7d321482c
miya10kei <miya10kei@gmail.com>
2020-02-12 23:49:43 +0900

    polish

--- src/AuthorizedUser.js
+++ src/AuthorizedUser.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react'
 import ReactRouterPropTypes from 'react-router-prop-types'
 import { withRouter } from 'react-router-dom'
+import { Mutation } from 'react-apollo'
+import { gql } from 'apollo-boost'
+import { ROOT_QUERY } from './App'
+
+const GITHUB_AUTH_MUTATION = gql`
+  mutation githubAuth($code: String!) {
+    githubAuth(code: $code) {
+      token
+    }
+  }
+`
 
 class AuthorizedUser extends Component {
   state = { signingIn: false }
 
+  authorizationComplete = (cache, { data }) => {
+    localStorage.setItem('token', data.githubAuth.token)
+    this.props.history.replace('/')
+    this.setState({ signingIn: false })
+  }
+
   componentDidMount() {
     if (window.location.search.match(/code=/)) {
       this.setState({ signingIn: true })
+      const code = window.location.search.replace('?code=', '')
+      this.githubAuthMutation({ variables: { code } })
     }
-    const code = window.location.search.replace('?code=', '')
-    // eslint-disable-next-line no-alert
-    alert(code)
-    this.props.history.replace('/')
   }
 
   requestCode() {
@@ -23,9 +38,20 @@ class AuthorizedUser extends Component {
 
   render() {
     return (
-      <button onClick={this.requestCode} disabled={this.state.signingIn}>
-        Sign In with GitHub
-      </button>
+      <Mutation
+        mutation={GITHUB_AUTH_MUTATION}
+        update={this.authorizationComplete}
+        refetchQueries={[{ query: ROOT_QUERY }]}
+      >
+        {mutation => {
+          this.githubAuthMutation = mutation
+          return (
+            <button onClick={this.requestCode} disabled={this.state.signingIn}>
+              Sign In with GitHub
+            </button>
+          )
+        }}
+      </Mutation>
     )
   }
 }
